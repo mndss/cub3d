61a62c4ebf30aea236e11f0ff8d4bdca7caa2cb7
Guilherme Martinelli <guilhermefmartinelli@gmail.com>
2022-03-24 16:43:59 -0300

    checking arguments errors

--- cub3d/includes/cub3d.h
+++ cub3d/includes/cub3d.h
@@ -1,10 +1,19 @@
 #ifndef CUB3D_H
 
 # define CUB3D_H
+# define ERROR_SIZE 3
 
 # include <mlx.h>
 # include <libft.h>
+# include <unistd.h>
+# include <fcntl.h>
 # include <stdio.h>
 # include <math.h>
+
+enum {
+	ERR_ARGS,
+	ERR_INVALID_EXT,
+	ERR_NOT_FOUND
+};
 
 #endif
--- cub3d/sources/main.c
+++ cub3d/sources/main.c
@@ -3,27 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: elima-me <elima-me@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: guferrei <guferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 16:57:10 by elima-me          #+#    #+#             */
-/*   Updated: 2022/03/21 14:24:41 by elima-me         ###   ########.fr       */
+/*   Updated: 2022/03/24 16:39:38 by guferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3d.h>
 
-int	ft_setup(int argc, char *argv[])
+int	print_err(int msg)
+{
+	char	*err[ERROR_SIZE];
+
+	err[ERR_ARGS] = "Error\nInvalid numbers of arguments\n";
+	err[ERR_INVALID_EXT] = "Error\nInvalid extension from map\n";
+	err[ERR_NOT_FOUND] = "Error\nFile not found\n";
+	ft_putstr_fd(err[msg], 2);
+	return (msg + 1);
+}
+
+int	invalid_args(int argc, char *argv[])
 {
 	if (argc != 2)
-	{
-		printf("Invalid numbers of arguments\n");
-		return (1);
-	}
+		return (print_err(ERR_ARGS));
 	if (ft_strncmp(&argv[1][strlen(argv[1]) - 4], ".cub", ft_strlen(argv[1])))
-	{
-		printf("Invalid extension from map\n");
-		return (2);
-	}
+		return (print_err(ERR_INVALID_EXT));
+	return (0);
+}
+
+int	ft_setup(int argc, char *argv[])
+{
+	if (invalid_args(argc, argv))
+		return (1);
 	return (0);
 }
 
