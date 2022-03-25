5be10d3e4847f9a6550a379ab80623a49a1bf8d9
Eduardo Mendes <eduardomendes.dev@gmail.com>
2022-03-25 17:19:26 -0300

    refactor: change argument's name

--- cub3d/libs/libft/ft_lstadd_back.c
+++ cub3d/libs/libft/ft_lstadd_back.c
@@ -6,19 +6,19 @@
 /*   By: elima-me <elima-me@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/05 17:56:41 by elima-me          #+#    #+#             */
-/*   Updated: 2021/06/07 15:38:48 by elima-me         ###   ########.fr       */
+/*   Updated: 2022/03/25 15:49:50 by elima-me         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd_back(t_list **lst, t_list *new)
+void	ft_lstadd_back(t_list **lst, t_list *newlst)
 {
 	t_list	*last_element;
 
 	last_element = ft_lstlast(*lst);
 	if (last_element != NULL)
-		last_element->next = new;
+		last_element->next = newlst;
 	else
-		*lst = new;
+		*lst = newlst;
 }
--- cub3d/libs/libft/ft_lstadd_front.c
+++ cub3d/libs/libft/ft_lstadd_front.c
@@ -6,16 +6,16 @@
 /*   By: elima-me <elima-me@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/04 16:28:22 by elima-me          #+#    #+#             */
-/*   Updated: 2021/06/05 18:58:42 by elima-me         ###   ########.fr       */
+/*   Updated: 2022/03/25 15:49:35 by elima-me         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd_front(t_list **lst, t_list *new)
+void	ft_lstadd_front(t_list **lst, t_list *newlst)
 {
 	if (!lst)
 		return ;
-	new->next = *lst;
-	*lst = new;
+	newlst->next = *lst;
+	*lst = newlst;
 }
